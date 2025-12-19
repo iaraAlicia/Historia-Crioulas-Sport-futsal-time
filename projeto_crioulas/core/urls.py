aa8addf6b9dc6dac0b81e030e18104982edaf384
Iara Alicia <iara44573@gmail.com>
2025-12-18 22:57:27 -0300

    resolvendo o conflito de rotas de core/urls

--- projeto_crioulas/core/urls.py
+++ projeto_crioulas/core/urls.py
@@ -2,17 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),       
-    path('historia/', views.historia, name='historia'), 
-<<<<<<< Updated upstream
-    # path('galeria/', views.galeria, name='galeria'),  
-    # path('conquistas/', views.conquistas, name='conquistas'),  
-    # path('contato/', views.contato, name='contato'),  
-=======
-    path('galeria/', views.galeria, name='galeria'),  
-    path('conquistas/', views.conquistas, name='conquistas'),  
-    path('contato/', views.contato, name='contato'),  
-    path('painel/', views.painel, name='painel'), # Painel Admin
-    path('deletar/<int:id>/', views.deletar_conquista, name='deletar'), # Rota para apagar
->>>>>>> Stashed changes
+    path('', views.home, name='home'),
 ]
